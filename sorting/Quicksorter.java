67b354875563a8c4caa29ba7a65dcc9a8dbe97bf
Isaac Hollander McCreery <ihmccreery@gmail.com>
2014-11-27 10:30:02 -0800

    Quicksorter 2

--- sorting/Quicksorter.java
+++ sorting/Quicksorter.java
@@ -1,44 +1,42 @@
 class Quicksorter {
     public static void sort(int[] a) {
-        quickSort(a, 0, a.length-1);
+        quicksort(a, 0, a.length - 1);
     }
 
-    // quicksort a[i] to a[k], inclusive
-    private static void quickSort(int[] a, int i, int k) {
+    // sort a from a[i] to a[k], inclusive
+    private static void quicksort(int[] a, int i, int k) {
         if (i < k) {
             int p = partition(a, i, k);
-            quickSort(a, i, p-1);
-            quickSort(a, p+1, k);
+            quicksort(a, i, p-1);
+            quicksort(a, p+1, k);
         }
     }
 
-    // partition a[i] to a[k], inclusive
+    // partition a from a[i] to a[k], inclusive
     private static int partition(int[] a, int i, int k) {
-        // get pivot value out of the way
-        swap(a, pivot(i, k), k);
-        // record pivotValue
-        int pivotValue = a[k];
-        // for j from i to k-1
-        int pivotLocation = i;
+        int p = pivot(i, k);
+        swap(a, p, k);
+        int pivot_value = a[k];
+        int pivot_index = i;
         for (int j = i; j < k; j++) {
-            if (a[j] < pivotValue) {
-                swap(a, pivotLocation, j);
-                pivotLocation++;
+            if (a[j] < pivot_value) {
+                swap(a, pivot_index, j);
+                pivot_index++;
             }
         }
-        swap(a, pivotLocation, k);
-        return pivotLocation;
+        swap(a, pivot_index, k);
+        return pivot_index;
     }
 
-    // swap elements i and j
+    // swap a[i] and a[j]
     private static void swap(int[] a, int i, int j) {
-        int t = a[i];
+        int tmp = a[i];
         a[i] = a[j];
-        a[j] = t;
+        a[j] = tmp;
     }
 
-    // choose a pivot: just stick to the last index for now
+    // partition a from a[i] to a[k], inclusive
     private static int pivot(int i, int k) {
-        return i + (k - i)/2;
+        return k;
     }
 }
