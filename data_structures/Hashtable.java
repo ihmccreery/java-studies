e988f402d6ed87fad5a940ddf6d4b288cdd9e523
Isaac Hollander McCreery <ihmccreery@gmail.com>
2014-11-29 18:24:26 -0800

    typographic edits to Hashtable

--- data_structures/Hashtable.java
+++ data_structures/Hashtable.java
@@ -1,23 +1,22 @@
 class Hashtable {
-
-    private boolean[] Store;
+    private boolean[] store;
 
     Hashtable() {
-        Store = new boolean[17];
-        for (int i = 0; i < Store.length; i++) {
-            Store[i] = false;
+        store = new boolean[17];
+        for (int i = 0; i < store.length; i++) {
+            store[i] = false;
         }
     }
 
     public void insert(int a) {
-        Store[hash(a)] = true;
+        store[hash(a)] = true;
     }
 
     public boolean lookup(int a) {
-        return Store[hash(a)];
+        return store[hash(a)];
     }
 
     private int hash(int a) {
-        return a % Store.length;
+        return a % store.length;
     }
 }
