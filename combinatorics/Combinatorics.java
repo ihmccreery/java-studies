f944a4394298ee7aa7d8a3ded6100e3a013e1282
Isaac Hollander McCreery <ihmccreery@gmail.com>
2014-11-30 17:33:50 -0800

    more combinatorics

--- combinatorics/Combinatorics.java
+++ combinatorics/Combinatorics.java
@@ -2,6 +2,16 @@ class Combinatorics {
     public static void main(String[] args) {
         System.out.println(chooseWrong(8, 3) == choose(8, 3));
         System.out.println(chooseWrong(20, 5) == choose(20, 5)); // choose overflows here
+        System.out.println("Probability of drawing a straight: " + pStraight());
+        System.out.println("Probability of drawing a pair: " + pPair());
+    }
+
+    private static double pStraight() {
+        return (9 * 5^4) / (double)choose(52, 5);
+    }
+
+    private static double pPair() {
+        return (13 * 4 * 3 * choose(12, 3) * 4^3) / (double)choose(52, 5);
     }
 
     private static int choose(int n, int k) {
