ab4c0f9ff633f48ac4642f9075595a76f837f28b
Isaac Hollander McCreery <ihmccreery@gmail.com>
2014-12-10 12:01:36 -0800

    AVL with correct height tracking and rebalancing

--- data_structures/AVL.java
+++ data_structures/AVL.java
@@ -1,84 +1,111 @@
-class AVL {
-    Data data;
-    int height;
-    AVL left;
-    AVL right;
+public class AVL<E extends Comparable<E>> {
+    Node root;
+    Sentinel sentinel;
 
     public AVL() {
-        height = 0;
+        this.root = this.sentinel = new Sentinel();
     }
 
-    public AVL insert(int a) {
-        if (data == null) {
-            data = new Data(a);
-            height = 1;
-            left = new AVL();
-            right = new AVL();
-            return this;
-        } else if (a < data.data) {
-            left = left.insert(a);
+    public void insert(E e) {
+        this.root = this.root.insert(e);
+    }
+
+    public boolean lookup(E e) {
+        return this.root.lookup(e);
+    }
+
+    private class Node {
+        E e;
+        Node l;
+        Node r;
+        int height;
+
+        Node() { }
+
+        Node(E e, Node l, Node r) {
+            this.e = e;
+            this.l = l;
+            this.r = r;
+            this.set_height();
+        }
+
+        Node insert(E e) {
+            if (e.compareTo(this.e) < 0) { // e < this.e
+                this.l = this.l.insert(e);
+            } else if (e.compareTo(this.e) > 0) { // e > this.e
+                this.r = this.r.insert(e);
+            } // else e == this.e
             return rebalance();
-        } else if (a > data.data) {
-            right = right.insert(a);
-            return rebalance();
-        } else { // a == data.data
-            return this;
+        }
+
+        private Node rebalance() {
+            int balance = this.balance();
+            if (balance > 1) { // R
+                if (this.r.balance() < 0) { // RL
+                    this.r = this.r.rotate_right(); // RL -> RR
+                } // RR
+                return this.rotate_left();
+            } else if (balance < -1) { // L
+                if (this.l.balance() > 0) { // LR
+                    this.l = this.l.rotate_left(); // LR -> LL
+                } // LL
+                return this.rotate_right();
+            } else {
+                this.set_height();
+                return this;
+            }
+        }
+
+        private int balance() {
+            return this.r.height - this.l.height;
+        }
+
+        private Node rotate_left() {
+            Node x = this.r;
+            Node y = x.l;
+            this.r = y;
+            x.l = this;
+            this.set_height();
+            x.set_height();
+            return x;
+        }
+
+        private Node rotate_right() {
+            Node x = this.l;
+            Node y = x.r;
+            this.l = y;
+            x.r = this;
+            this.set_height();
+            x.set_height();
+            return x;
+        }
+
+        private void set_height() {
+            this.height = 1 + Math.max(this.l.height, this.r.height);
+        }
+
+        boolean lookup(E e) {
+            if (e.compareTo(this.e) < 0) { // e < this.e
+                return this.l.lookup(e);
+            } else if (e.compareTo(this.e) > 0) { // e > this.e
+                return this.r.lookup(e);
+            } else { // e == this.e
+                return true;
+            }
         }
     }
 
-    private AVL rebalance() {
-        if (lean() > 1) { // left
-            if (left.lean() < 0) { // left-right, correct to left-left
-                left = left.rotate_left();
-            }
-            return rotate_left();
-        } else if (lean() < -1) { // right
-            if (right.lean() > 0) { // right-left, correct to right-right
-                right = right.rotate_right();
-            }
-            return rotate_right();
-        } else {
-            return this;
+    private class Sentinel extends Node {
+        Sentinel() {
+            this.height = -1;
         }
-    }
 
-    private AVL rotate_left() {
-        AVL x = this.right;
-        AVL y = x.left;
-        x.left = this;
-        this.right = y;
-        return x;
-    }
+        Node insert(E e) {
+            return new Node(e, this, this);
+        }
 
-    private AVL rotate_right() {
-        AVL x = this.left;
-        AVL y = x.right;
-        x.right = this;
-        this.left = y;
-        return x;
-    }
-
-    private int lean() {
-        return left.height - right.height;
-    }
-
-    public boolean lookup(int a) {
-        if (data == null) {
+        boolean lookup(E e) {
             return false;
-        } else if (a < data.data) {
-            return left.lookup(a);
-        } else if (a > data.data) {
-            return right.lookup(a);
-        } else { // a == data.data
-            return true;
-        }
-    }
-
-    private class Data {
-        int data;
-
-        Data(int a) {
-            data = a;
         }
     }
 }
--- data_structures/Lookup.java
+++ data_structures/Lookup.java
@@ -1,13 +1,18 @@
 class Lookup {
     public static void main(String[] args) {
-        Hashtable t = new Hashtable();
-        int[] nums = {1, 2, 4, 5, 6, 8, 11, 14, 12, 10, 5, 5, 1, 0};
+        AVL<Integer> t = new AVL<Integer>();
+        int[] nums = {2, 8, 4, 6, 14, 12, 10, 24, 60, 8, 16, 32, 24, 52, 80, 40, 18, 0};
 
         for (int i = 0; i < nums.length; i++) {
             t.insert(nums[i]);
         }
 
-        System.out.println(t.lookup(6) + " should be true.");
-        System.out.println(t.lookup(13) + " should be false.");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(t.lookup(nums[i]) + " should be true.");
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(t.lookup(nums[i]+1) + " should be false.");
+        }
     }
 }
